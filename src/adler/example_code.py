d52fe52d2ce44a6073a4cfa96bdd01597edfd22c
Steph Merritt <s.merritt@qub.ac.uk>
2024-01-29 12:01:59 +0000

    Adding test code.

---
+++ src/adler/example_code.py
@@ -0,0 +1,2 @@
+def test_function(x):
+    return x * x
---
+++ tests/adler/test_example.py
@@ -0,0 +1,6 @@
+def test_test_function():
+    from adler.example_code import test_function
+
+    test_value = test_function(2)
+
+    assert test_value == 4
