9a900d9244a592a4510c8e732979088aa7572d1f
Steph Merritt <s.merritt@qub.ac.uk>
2024-03-08 11:19:06 +0000

    Refactor and adding filters.

---
+++ src/adler/dataclasses/MPCORB.py
@@ -0,0 +1,119 @@
+from dataclasses import dataclass
+
+from adler.dataclasses.dataclass_utilities import get_from_table
+
+
+@dataclass
+class MPCORB:
+    """Object information from MPCORB. All attributes carry the same names as the column names from the MPCORB table.
+
+    Attributes:
+    -----------
+    ssObjectId: str
+        LSST unique identifier (if observed by LSST)
+
+    mpcDesignation: str
+        Number or provisional designation (in packed form)
+
+    mpcNumber: int
+        MPC number (if the asteroid has been numbered; NULL otherwise). Provided for convenience.
+
+    mpcH: float
+        Absolute magnitude, H
+
+    mpcG: float
+        Slope parameter, G
+
+    epoch: float
+        Epoch (in MJD, .0 TT)
+
+    peri: float
+        Argument of perihelion, J2000.0 (degrees)
+
+    node: float
+        Longitude of the ascending node, J2000.0 (degrees)
+
+    incl: float
+        Inclination to the ecliptic, J2000.0 (degrees)
+
+    e: float
+        Orbital eccentricity
+
+    n: float
+        Mean daily motion (degrees per day)
+
+    q: float
+        Perihelion distance (AU)
+
+    uncertaintyParameter: str
+        Uncertainty parameter, U
+
+    flags: str
+        4-hexdigit flags. See https://minorplanetcenter.net//iau/info/MPOrbitFormat.html for details
+
+    """
+
+    ssObjectId: str = ""
+    mpcDesignation: str = ""
+    mpcNumber: int = 0
+    mpcH: float = 0.0
+    mpcG: float = 0.0
+    epoch: float = 0.0
+    peri: float = 0.0
+    node: float = 0.0
+    incl: float = 0.0
+    e: float = 0.0
+    n: float = 0.0
+    q: float = 0.0
+    uncertaintyParameter: str = ""
+    flags: str = ""
+
+    @classmethod
+    def construct_from_data_table(cls, ssObjectId, data_table):
+        """Initialises the MPCORB object from a table of data.
+
+        Parameters
+        -----------
+        ssObjectId : str
+            ssObjectId of the object of interest.
+
+        data_table : table-like object
+            Table of data from which attributes shoud be populated.
+
+        Returns
+        -----------
+        MPCORB object
+            MPCORB object with class attributes populated from data_table.
+
+        """
+
+        mpcDesignation = get_from_table(data_table, "mpcDesignation", "str")
+        mpcNumber = get_from_table(data_table, "mpcNumber", "int")
+        mpcH = get_from_table(data_table, "mpcH", "float")
+        mpcG = get_from_table(data_table, "mpcH", "float")
+        epoch = get_from_table(data_table, "epoch", "float")
+        peri = get_from_table(data_table, "peri", "float")
+        node = get_from_table(data_table, "node", "float")
+        incl = get_from_table(data_table, "incl", "float")
+        e = get_from_table(data_table, "e", "float")
+        n = get_from_table(data_table, "n", "float")
+        q = get_from_table(data_table, "q", "float")
+        uncertaintyParameter = get_from_table(data_table, "uncertaintyParameter", "str")
+        flags = get_from_table(data_table, "flags", "str")
+
+        return cls(
+            ssObjectId,
+            mpcDesignation,
+            mpcNumber,
+            mpcH,
+            mpcG,
+            epoch,
+            peri,
+            node,
+            incl,
+            e,
+            n,
+            q,
+            uncertaintyParameter,
+            flags,
+        )
---
+++ src/adler/dataclasses/SSObject.py
@@ -0,0 +1,128 @@
+from dataclasses import dataclass, field
+import numpy as np
+
+from adler.dataclasses.dataclass_utilities import get_from_table
+
+
+@dataclass
+class SSObject:
+    """Object information from SSObject. All attributes carry the same names as the column names from the SSObject table.
+
+    Attributes:
+    -----------
+
+    ssObjectId: str
+        LSST unique identifier
+
+    filter_list : list of str
+        A comma-separated list of the filters of interest.
+
+    discoverySubmissionDate : float
+        The date the LSST first linked and submitted the discovery observations to the MPC. May be NULL if not an LSST discovery. The date format will follow general LSST conventions (MJD TAI, at the moment).
+
+    firstObservationDate: float
+        The time of the first LSST observation of this object (could be precovered)
+
+    arc: float
+        Arc of LSST observations
+
+    numObs: int
+        Number of LSST observations of this object
+
+    H: array_like of floats
+        Best fit absolute magnitudes per-filter, in same order as filter_list.
+
+    G12: array_like of floats
+        Best fit G12 slope parameters per-filter, in same order as filter_list.
+
+    Herr : array_like of floats
+        Uncertainty of H per-filter, in same order as filter_list.
+
+    G12Err : array_like of floats
+        Uncertainty of G12 per-filter, in same order as filter_list.
+
+    nData: array_like of ints
+        The number of data points used to fit the phase curve per-filter, in same order as filter_list.
+
+    maxExtendedness: float
+        maximum `extendedness` value from the DIASource
+
+    minExtendedness: float
+        minimum `extendedness` value from the DIASource
+
+    medianExtendedness: float
+        median `extendedness` value from the DIASource
+
+    """
+
+    ssObjectId: str = ""
+    filter_list: list = field(default_factory=list)
+    discoverySubmissionDate: float = 0.0
+    firstObservationDate: float = 0.0
+    arc: float = 0.0
+    numObs: int = 0
+    H: np.ndarray = field(default_factory=np.zeros(0))
+    G12: np.ndarray = field(default_factory=np.zeros(0))
+    Herr: np.ndarray = field(default_factory=np.zeros(0))
+    G12err: np.ndarray = field(default_factory=np.zeros(0))
+    nData: np.ndarray = field(default_factory=np.zeros(0))
+    maxExtendedness: float = 0.0
+    minExtendedness: float = 0.0
+    medianExtendedness: float = 0.0
+
+    @classmethod
+    def construct_from_data_table(cls, ssObjectId, filter_list, data_table):
+        discoverySubmissionDate = get_from_table(data_table, "discoverySubmissionDate", "float")
+        firstObservationDate = get_from_table(data_table, "firstObservationDate", "float")
+        arc = get_from_table(data_table, "arc", "float")
+        numObs = get_from_table(data_table, "numObs", "int")
+
+        H = np.zeros(len(filter_list))
+        G12 = np.zeros(len(filter_list))
+        Herr = np.zeros(len(filter_list))
+        G12err = np.zeros(len(filter_list))
+        nData = np.zeros(len(filter_list))
+
+        for i, filter in enumerate(filter_list):
+            H[i] = get_from_table(data_table, filter + "_H", "float")
+            G12[i] = get_from_table(data_table, filter + "_G12", "float")
+            Herr[i] = get_from_table(data_table, filter + "_Herr", "float")
+            G12err[i] = get_from_table(data_table, filter + "_G12err", "float")
+            nData[i] = get_from_table(data_table, filter + "_nData", "int")
+
+        maxExtendedness = get_from_table(data_table, "maxExtendedness", "float")
+        minExtendedness = get_from_table(data_table, "minExtendedness", "float")
+        medianExtendedness = get_from_table(data_table, "medianExtendedness", "float")
+
+        return cls(
+            ssObjectId,
+            filter_list,
+            discoverySubmissionDate,
+            firstObservationDate,
+            arc,
+            numObs,
+            H,
+            G12,
+            Herr,
+            G12err,
+            nData,
+            maxExtendedness,
+            minExtendedness,
+            medianExtendedness,
+        )
+
+    def populate_from_table(self, data_table):
+        """Populates the SSObject object from the data_table class variable created on initialisation."""
+
+        self.discoverySubmissionDate = self.get_from_table(data_table, "discoverySubmissionDate", "float")
+        self.firstObservationDate = self.get_from_table(data_table, "firstObservationDate", "float")
+        self.arc = self.get_from_table(data_table, "arc", "float")
+        self.numObs = self.get_from_table(data_table, "numObs", "int")
+        self.r_H = self.get_from_table(data_table, "r_H", "float")
+        self.r_G12 = self.get_from_table(data_table, "r_G12", "float")
+        self.r_Herr = self.get_from_table(data_table, "r_Herr", "float")
+        self.r_G12Err = self.get_from_table(data_table, "r_G12err", "float")
+        self.r_nData = self.get_from_table(data_table, "r_nData", "int")
+        self.maxExtendedness = self.get_from_table(data_table, "maxExtendedness", "float")
+        self.minExtendedness = self.get_from_table(data_table, "minExtendedness", "float")
+        self.medianExtendedness = self.get_from_table(data_table, "medianExtendedness", "float")
