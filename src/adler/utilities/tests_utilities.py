5a0f280e5a22a511a55419727a9fbb5cff41ea6c
Steph Merritt <s.merritt@qub.ac.uk>
2024-03-26 14:38:03 +0000

    docstrings for test utility functions

--- src/adler/utilities/tests_utilities.py
+++ src/adler/utilities/tests_utilities.py
@@ -2,7 +2,16 @@ import os
 from pathlib import Path
 
 
-def get_test_data_path():
+def get_test_data_path():  # pragma: no cover
+    """Gets the absolute path of the tests/data directory.
+
+    Returns
+    -----------
+    path_to_data : str
+        The absolute path to the tests/data directory.
+
+    """
+
     # where is this file?
     path_to_file = os.path.abspath(__file__)
 
@@ -12,7 +21,22 @@ def get_test_data_path():
     return path_to_data
 
 
-def get_test_data_filepath(filename):
+def get_test_data_filepath(filename):  # pragma: no cover
+    """Gets the absolute path of a supplied file in the tests/data directory.
+
+
+    Parameters
+    -----------
+    filename : str
+        The filename of the desired test file.
+
+    Returns
+    -----------
+    str
+        The absolute path to the specified file in the tests/data directory.
+
+    """
+
     filepath = get_test_data_path()
 
     return os.path.join(filepath, filename)
