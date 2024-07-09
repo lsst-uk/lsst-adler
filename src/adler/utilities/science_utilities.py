d32d15c48fb422399d7bdf2eb1225e0b5ce9a20c
jrob93 <jrobinson72@qub.ac.uk>
2024-07-09 12:34:30 +0100

    get obs as df func

--- src/adler/utilities/science_utilities.py
+++ src/adler/utilities/science_utilities.py
@@ -1,5 +1,7 @@
 import numpy as np
+import pandas as pd
 from astropy.stats import sigma_clip as astropy_sigma_clip
+import astropy.units as u
 
 
 def outlier_diff(new_res, diff_cut=1.0):
@@ -158,3 +160,64 @@ def apparition_gap_finder(x, dx=100.0):
     x_gaps = np.insert(x_gaps, len(x_gaps), x[-1] + dx)
 
     return x_gaps
+
+
+def get_df_obs_filt(planetoid, filt, x_col="midPointMjdTai", x1=None, x2=None, col_list=None, pc_model=None):
+    """Retrieve a dataframe of observations in a given filter. Has the option to limit the observations to a range of values, e.g. times/phase angles, if required.
+
+    Parameters
+    -----------
+
+    planetoid: object
+        Adler planetoid object containging the observations
+    filt: str
+        The filter to query
+    x_col: str
+        Column name to use for ordering values and limiting observations to a range: x1 <= df_obs[x_col] <= x2
+    x1: float
+        Lower limit value for x_col
+    x2: float
+        Upper limit value for x_col
+    col_list: list
+        List of column names to retrieve in addition to x_col, otherwise all columns are retrieved.
+        N.B. if AbsMag is included in col_list then the absolute magnitude is calculated for the phase curve model pc_model
+    pc_model: object
+        Adler PhaseCurve model used to calculate AbsMag if required
+
+    Returns
+    -----------
+    df_obs: DataFrame
+        DataFrame of observations in the requested filter, ordered by x_col and with any x_col limits applied
+    """
+
+    # get observations in filter as a dataframe
+    obs = planetoid.observations_in_filter(filt)
+    # TODO: add option to get all filters? calculating AbsMag gets complicated...
+    # TODO: split the dataframe functions into a separate function?
+    df_obs = pd.DataFrame(obs.__dict__)
+
+    # if no list of columns is provided, retrieve all columns
+    if col_list is None:
+        col_list = list(df_obs)
+    else:
+        col_list = [x_col] + col_list
+
+    # calculate the phase curve absolute magnitudes (i.e. remove phase curve variation from reduced_mag)
+    if "AbsMag" in col_list:
+        # calculate the model absolute magnitude
+        # TODO: add robustness to the units, phaseAngle and reduced_mag must match pc_model
+        df_obs["AbsMag"] = pc_model.AbsMag(obs.phaseAngle * u.deg, obs.reduced_mag * u.mag).value
+
+    # select only the required columns
+    df_obs = df_obs[col_list]
+
+    # sort into x_col order
+    df_obs = df_obs.sort_values(x_col)
+    # limit to range in x_col if limits x1, x2 are supplied
+    if (x1 is not None) & (x2 is not None):
+        x_mask = (df_obs[x_col] >= x1) & (df_obs[x_col] <= x2)
+        df_obs = df_obs[x_mask]
+    # reset the dataframe index
+    df_obs = df_obs.reset_index(drop=True)
+
+    return df_obs
