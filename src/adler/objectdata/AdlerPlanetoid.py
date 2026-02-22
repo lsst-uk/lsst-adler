b9aff05b6fd2c82053d07a7b2d8da148120de8b6
Michael S. P. Kelley <msk@astro.umd.edu>
2026-02-22 08:25:57 -0500

    Add filter, return sorted observations.

--- src/adler/objectdata/AdlerPlanetoid.py
+++ src/adler/objectdata/AdlerPlanetoid.py
@@ -990,11 +990,13 @@ class AdlerPlanetoid:
 
         result = pd.DataFrame()
         for obs in self.observations_by_filter:
-            df = pd.DataFrame(obs.__dict__)
+            df = pd.DataFrame(obs.__dict__ | {"filter_name": [obs.filter_name] * obs.num_obs})
             result = pd.concat([result, df]).reset_index(drop=True)
 
-        i = (obs.midPointMjdTai >= start) * (obs.midPointMjdTai <= stop)
-        return result.iloc[i]
+        i = (result.midPointMjdTai >= start) * (result.midPointMjdTai <= stop)
+        result = result.iloc[i]
+        result.sort_values("midPointMjdTai")
+        return result
 
     def SSObject_in_filter(self, filter_name):
         """User-friendly helper function. Returns the filter-dependent values from SSObject for a given filter.
