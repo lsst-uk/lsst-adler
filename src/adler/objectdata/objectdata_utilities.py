d11f2e998625e77db9d05072363432057a7c7b60
jrob93 <james.robinson@ed.ac.uk>
2026-09-01 16:08:01 +0000

    update service and async queries

--- src/adler/objectdata/objectdata_utilities.py
+++ src/adler/objectdata/objectdata_utilities.py
@@ -47,7 +47,18 @@ def get_data_table(sql_query, service=None, sql_filename=None):
     """
 
     if service:  # pragma: no cover
-        data_table = service.search(sql_query)
+        # data_table = service.search(sql_query) # sync queries will time out...
+        
+        # run the query async
+        job = service.submit_job(query)
+        job.run()
+        job.wait(phases=['COMPLETED', 'ERROR'])
+        print('Job phase is', job.phase)
+        if job.phase == 'ERROR':
+            job.raise_if_error()
+        assert job.phase == 'COMPLETED'
+        data_table = job.fetch_result()
+        
     elif sql_filename:
         cnx = sqlite3.connect(sql_filename)
         data_table = pd.read_sql_query(
